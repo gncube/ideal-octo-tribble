8511b2692a47fd7146b61f9a3a316833669317f0
Gerald Ncube <4821668+gncube@users.noreply.github.com>
2024-09-18 23:06:10 +0200

    Initial commit

---
+++ Services/Interfaces/ITaskerService.cs
@@ -0,0 +1,15 @@
+﻿using BlazorWasmComponentDemo.Models;
+
+namespace BlazorWasmComponentDemo.Services.Interfaces
+{
+    public interface ITaskerService
+    {
+        Task<List<TaskerItem>> GetTaskerItemsAsync();
+
+        Task SaveTaskerItemsAsync(List<TaskerItem> taskList);
+
+        Task AddTaskerItem(TaskerItem item);
+
+        Task RemoveTaskerItem(Guid id);
+    }
+}
---
+++ Services/TaskerService.cs
@@ -0,0 +1,75 @@
+﻿using BlazorWasmComponentDemo.Models;
+using BlazorWasmComponentDemo.Services.Interfaces;
+using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+using System.Diagnostics;
+using System.Text.Json;
+
+
+namespace BlazorWasmComponentDemo.Services
+{
+
+    /// <summary>
+    /// Service for managing tasker items.
+    /// </summary>
+    public class TaskerService(IJSRuntime jsRuntime) : ITaskerService
+    {
+       
+        /// <summary>
+        /// Adds a new tasker item.
+        /// </summary>
+        /// <param name="item">The tasker item to add.</param>
+        public async Task AddTaskerItem(TaskerItem item)
+        {
+            item.Id = Guid.NewGuid();
+            item.IsComplete = false;
+
+            List<TaskerItem> newTaskList = (await GetTaskerItemsAsync()).ToList();
+            newTaskList.Add(item);
+
+            await SaveTaskerItemsAsync(newTaskList);
+        }
+
+        /// <summary>
+        /// Removes a tasker item by its ID.
+        /// </summary>
+        /// <param name="id">The ID of the tasker item to remove.</param>
+        public async Task RemoveTaskerItem(Guid id)
+        {
+            List<TaskerItem> taskList = (await GetTaskerItemsAsync()).ToList();
+            taskList = taskList.Where(t => t.Id != id).ToList();
+
+            await SaveTaskerItemsAsync(taskList);
+        }
+
+        /// <summary>
+        /// Retrieves all tasker items.
+        /// </summary>
+        /// <returns>A list of tasker items.</returns>
+        public async Task<List<TaskerItem>> GetTaskerItemsAsync()
+        {
+            try
+            {
+                string? taskListState = await jsRuntime.InvokeAsync<string>("localStorage.getItem", "TaskerList") ?? "[]";
+                List<TaskerItem> taskList = JsonSerializer.Deserialize<List<TaskerItem>>(taskListState)!;
+                return taskList;
+            }
+            catch (Exception)
+            {
+                List<TaskerItem> taskList = new List<TaskerItem>();
+                await SaveTaskerItemsAsync(taskList);
+                return taskList;
+            }
+        }
+
+        /// <summary>
+        /// Saves the tasker items.
+        /// </summary>
+        /// <param name="taskList">The list of tasker items to save.</param>
+        public async Task SaveTaskerItemsAsync(List<TaskerItem> taskList)
+        {
+            string? taskListState = JsonSerializer.Serialize(taskList);
+            await jsRuntime.InvokeVoidAsync("localStorage.setItem", "TaskerList", taskListState);
+        }
+    }
+}
